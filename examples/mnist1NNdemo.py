779eab9af51df222f6643c791e34d26401c5cd04
Lee Zamparo <zamparo@gmail.com>
2017-06-20 14:47:49 -0400

    added code to grab and process MNIST data, also set data subfolder to be ignored (so changes below should not be added to the repo)

--- examples/mnist1NNdemo.py
+++ examples/mnist1NNdemo.py
@@ -2,44 +2,111 @@
 
 from scipy import sparse
 import numpy as np
+from array import array
 
 import struct
 import os
 import time
+import gzip
+
+MNIST_prefix="http://yann.lecun.com/exdb/mnist/"
+
 
 #This PULLMNIST function is from https://gist.github.com/akesling/5358964
-def PullMNIST(dataset = "training", path = "."):
+
+def PullMNIST(dataset = "training", source_path = ".", save_dir = None):
     """
     Python function for importing the MNIST data set.  It returns an iterator
     of 2-tuples with the first element being the label and the second element
     being a numpy.uint8 2D array of pixel data for the given image.
+    
+    dataset (string): one of 'training' or 'testing'
+    
+    source_path (string): location of MNIST data set.  Can be a URL if you are 
+    fetching the data for the first time, or a local path where the data resides
+    
+    save_dir (string): [optional]  the local path where you'd like to save the 
+    MNIST data if fetching it from a URL
     """
 
     if dataset is "training":
-        fname_img = os.path.join(path, 'train-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 'train-labels-idx1-ubyte')
+        fname_img = os.path.join(source_path, 'train-images-idx3-ubyte.gz')
+        fname_lbl = os.path.join(source_path, 'train-labels-idx1-ubyte.gz')
     elif dataset is "testing":
-        fname_img = os.path.join(path, 't10k-images-idx3-ubyte')
-        fname_lbl = os.path.join(path, 't10k-labels-idx1-ubyte')
+        fname_img = os.path.join(source_path, 't10k-images-idx3-ubyte.gz')
+        fname_lbl = os.path.join(source_path, 't10k-labels-idx1-ubyte.gz')
     else:
         raise ValueError("dataset must be \'testing\' or \'training\'")
 
-    # Load everything in some numpy arrays
-    with open(fname_lbl, 'rb') as flbl:
-        magic, num = struct.unpack(">II", flbl.read(8))
-        lbl = np.fromfile(flbl, dtype=np.int8)
+    # Take care of gzipped files transparently
+    if fname_lbl.endswith('gz') and fname_img.endswith('gz'):
+        myopen = gzip.open
+    else:
+        myopen = open    
 
-    with open(fname_img, 'rb') as fimg:
-        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
-        img = np.fromfile(fimg, dtype=np.uint8).reshape(len(lbl), rows, cols)
+    
+    if fname_lbl.startswith("http") and fname_lbl.startswith("http"):
+        
+        # Fetch files over http, save, convert, return np arrays
+        if not os.path.exists(save_dir):
+            os.mkdir(save_dir)
+        
+        import urllib.request
+        labels, info = urllib.request.urlretrieve(fname_lbl, os.path.join(os.path.dirname(fname_lbl), save_dir, os.path.basename(fname_lbl)))      
+        images, info = urllib.request.urlretrieve(fname_img, os.path.join(os.path.dirname(fname_img), save_dir, os.path.basename(fname_img)))
+           
+        with myopen(os.path.join(save_dir, os.path.basename(fname_lbl)), 'rb') as flbl:
+            magic, num = struct.unpack(">II", flbl.read(8))
+            my_lbl = array("B", flbl.read())
+            lbl = np.array(my_lbl, dtype=np.int8)
+    
+        with myopen(os.path.join(save_dir, os.path.basename(fname_img)), 'rb') as fimg:
+            magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            my_img = array("B", fimg.read())
+            
+            images = []
+            for i in range(num):
+                images.append([0] * rows * cols)
+        
+            for i in range(num):
+                images[i][:] = my_img[i * rows * cols:(i + 1) * rows * cols]
+                    
+            img = np.array(images)       
+        
+    else:
+        
+        # Load from local filesystem, convert, return np arrays
+        with myopen(fname_lbl, 'rb') as flbl:
+            magic, num = struct.unpack(">II", flbl.read(8))
+            my_lbl = array("B", flbl.read())
+            lbl = np.array(my_lbl, dtype=np.int8)
+    
+        with myopen(fname_img, 'rb') as fimg:
+            magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+            my_img = array("B", fimg.read())
+            
+            images = []
+            for i in range(num):
+                images.append([0] * rows * cols)
+        
+            for i in range(num):
+                images[i][:] = my_img[i * rows * cols:(i + 1) * rows * cols]
+                    
+            img = np.array(images)
 
     return {'labels':lbl,'images':img}
         
-Training = PullMNIST("training",'/Data/MNIST')
+# Pull the MNIST data from yann.lecun.com
+
+#Training = PullMNIST("training", MNIST_prefix, os.path.expanduser("~/projects/pyprobml/data/MNIST"))
+#Testing = PullMNIST("testing", MNIST_prefix, os.path.expanduser("~/projects/pyprobml/data/MNIST"))
+
+# Pull the MNIST data from a local dir
+Training = PullMNIST("training", os.path.expanduser("~/projects/pyprobml/data/MNIST"))
 TrainLabels = Training['labels']
 TrainIms = Training['images'].astype('int32')
 
-Testing = PullMNIST("testing",'/Data/MNIST')
+Testing = PullMNIST("testing", os.path.expanduser("~/projects/pyprobml/data/MNIST"))
 TestLabels = Testing['labels']
 TestIms = Testing['images'].astype('int32')
 
